7858afd88d13e3915f555051383369351ab9d9ce
Abel Ccopa <abel.ccopa@tecsup.edu.pe>
2022-09-13 17:16:16 -0500

    primera version

---
+++ Actividad_01.js
@@ -0,0 +1,15 @@
+
+var nombre = "Carlos";
+console.log(nombre);
+
+
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato object
+var objecto = {
+    nombre:"Juan",
+    apellido: "Perez",
+    telefono: "55443322",
+}
+console.log(objecto);
---
+++ Actividad_03.js
@@ -0,0 +1,7 @@
+var autos = ['BMW','Audi','Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+
+var z = '';
+console.log(z);
